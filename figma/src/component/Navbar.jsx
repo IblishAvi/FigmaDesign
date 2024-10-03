90da1af3c100f59e89b75ca25b00dc15f396c88a
Avinash Jha <127582620+IblishAvi@users.noreply.github.com>
2024-10-03 21:59:33 +0530

    Update Navbar.jsx

--- figma/src/component/Navbar.jsx
+++ figma/src/component/Navbar.jsx
@@ -9,10 +9,10 @@ function Navbar() {
     <>
       <div className='h-10 bg-white flex w-full justify-between overflow-hidden '>
       <div className='w-24 h-10 '>
-            <img className='w-24 pl-0' src="./images/Valle.png" alt="VALLE" />
+            <img className='w-24 pl-0' src="./Valle.png" alt="VALLE" />
       </div>
       <div className='w-32 h-10 flex justify-start'>
-            <img className=' rounded-full' src="./images/img2.webp" alt="" />
+            <img className=' rounded-full' src="./img2.webp" alt="" />
             <select  className='h-10 align-baseline '>
                 <option value="John Doe">John Doe</option>
             </select>
